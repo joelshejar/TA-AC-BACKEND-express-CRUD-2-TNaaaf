665177bcc1f151d7d1fe67eedc22c8a8b487d02d
abc <joel.rajesh13@gmail.com>
2021-07-08 07:49:38 +0530

    Assignment 1 blog refactor

--- block-BNaadl/blog/routes/articles.js
+++ block-BNaadl/blog/routes/articles.js
@@ -14,16 +14,28 @@ router.get('/', function(req, res, next) {
 // fetch single article
 router.get('/:id', (req,res,next)=>{
   var id = req.params.id
+  Article.findById(id, (err, article) => {
+    if(err) return next(err);
+    res.render('singleArticle', { article: article});
+});
 })
 
 // create article
 router.post('/',(req,res,next)=>{
   req.body.tags = req.body.tags.trim().split(" ")
+  Article.create(req.body,  (err, createdArticle) => {
+    if(err) return next(err);
+    res.redirect('/articles');
+});   
 })
 
 // edit article form
 router.get('/:id/edit', (req,res,next)=>{
   var id = req.params.id
+  Article.findById(id, (err, article) =>  {
+    if(err) return next(err);
+    res.render('editArticle', { article });
+});
 })
 
 // create article form
@@ -35,7 +47,21 @@ router.get('/new',(req,res)=>{
 
 router.get('/:id', (req,res,next)=>{
   var id = req.params.id
+  Article.findById(id, (err, article) => {
+    if(err) return next(err);
+    res.render('singleArticle', { article: article});
+});
 })
+
+// Increment Likes
+router.get('/:id/likes', (req, res, next) => {
+  let id = req.params.id;
+
+  Article.findByIdAndUpdate(id, {$inc: {likes: 1}}, (err, article) =>  {
+      if(err) return next(err);
+      res.redirect('/articles/' + id);
+  });
+});
 
 // create article
 router.post('/', (req,res,next)=>{
@@ -55,12 +81,33 @@ router.get('/:id/edit',(req,res,next)=>{
 // update article
 router.post('/:id', (req,res)=>{
   var id = req.params.id
-  req.body.tags = req.body.tags.split(" ")
+  req.body.tags = req.body.tags.trim().split(' ');
+    Article.findByIdAndUpdate(id, req.body, (err, updatedArticle) => {
+        if(err) return next(err);
+        res.redirect('/articles');
+    });
 })
+router.put('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Article.findByIdAndUpdate(id, req.body, (err, updatedArticle) => {
+      if(err) return next(err);
+      res.redirect('/articles');
+  });
+});
 
 // delete article
-router.get('/:id/delete', (req,res,next)=>{
-
-})
-
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id;
+  Article.findByIdAndDelete(id, (err, deletedArticle) => {
+      if(err) return next(err);
+      res.redirect('/articles');
+  });
+});
+router.get('/:id/delete', (req, res, next) => {
+  let id = req.params.id;
+  Article.findByIdAndDelete(id, (err, deletedArticle) => {
+      if(err) return next(err);
+      res.redirect('/articles');
+  });
+});
 module.exports = router;
